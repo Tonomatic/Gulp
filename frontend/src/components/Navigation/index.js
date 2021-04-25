9e909266428ef78fd6c464a5f1fd5279b5c17314
jose <antonysoly2217@gmail.com>
2021-04-25 01:38:06 -0400

    MoreChanges

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -26,6 +26,7 @@ function Navigation({ isLoaded }) {
             <li>
                 <NavLink exact to="/">Home</NavLink>
                 {isLoaded && sessionLinks}
+                {/* <p>THIS IS A TEST</p> */}
             </li>
         </ul>
     );
