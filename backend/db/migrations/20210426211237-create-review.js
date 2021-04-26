6f127dce960bfeb302902caefaf2003e246e8a06
jose <antonysoly2217@gmail.com>
2021-04-26 17:12:55 -0400

    Databases

---
+++ backend/db/migrations/20210426211237-create-review.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewText: {
+        type: Sequelize.STRING
+      },
+      reviewPhotos: {
+        type: Sequelize.TEXT
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      reviewMark: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      restaurantId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Reviews');
+  }
+};
